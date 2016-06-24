7d782b9dad248eb3745962127688109be5742ebe
Frank Paschen <frankpaschen99@gmail.com>
2016-06-24 03:04:50 -0400

    changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(),
     io = require('socket.io')(server),
     logger = require('winston'),
     port = 8080;
-	
+
 var numConnected = 0;
 
 logger.remove(logger.transports.Console);
@@ -15,43 +15,31 @@ logger.add(logger.transports.Console, {
 logger.info('Server > listening on port ' + port);
 io.on('connection', function(socket) {
     logger.info('Server > Connected socket ' + socket.id);
-	numConnected++
-	console.log("Clients Connected: " + numConnected);
+    numConnected++
+    console.log("Clients Connected: " + numConnected);
     socket.on('broadcast', function(message) {
-		console.log(typeof message);
-		console.log(message);
         logger.info('CSGOLotus broadcast > ' + JSON.stringify(message));
         io.emit('gamedata', message)
     });
-	socket.on('response', function(message) {
-		logger.info('Scruffybot response > ' + message);
-		
-		var json = JSON.parse(message);
-		
-		io.emit('sendtrade', json.tradeID);
-		
-		var SID64 = json.SID;
-		var tradeID = json.tradeID;
-		var assetids = json.items;
-		var bot_inventory = "http://steamcommunity.com/id/thescruffybot/inventory/json/730/2"; // this will be more dynamic when we have multiple bots	
-		
-		// here, the bot will take the assetids provided by the steambot, and compare them to the inventory JSON of the player
-		// using their STEAMID64, and take the original_ids that correspond.
-		// then the bot will need to take the original_ids and compare them to the 
-		// original_ids in scruffybot's inventory, then take the assetid that it corresponds to, use that to
-		// fetch the market_hash_name, then use that to fetch the $$ value from the BP.TF schema.
-		
-		// this is the url needed to compare to scruffybot's original_ids
-		// https://api.steampowered.com/IEconItems_730/GetPlayerItems/v1/?key=2457B1C97418CC3095E99484AF2DC660&steamid=76561198180102897
-		// this is the url needed to compare the assetids to find the market_hash_name
-		// http://steamcommunity.com/id/thescruffybot/inventory/json/730/2
-		// BP.TF schema url for skin values (will later be on our file server, being updated every 5 minutes)
-		// http://backpack.tf/api/IGetMarketPrices/v1/?key=56cd0ca5b98d88be2ef9de16&appid=730
-	});
+    socket.on('response', function(message) {
+        logger.info('Scruffybot response > ' + message);
+
+        var json = JSON.parse(message);
+
+        var SID64 = json.SID;
+        var assetids = json.items;
+        var bot_inventory = "http://steamcommunity.com/id/thescruffybot/inventory/json/730/2"; // this will be more dynamic when we have multiple bots
+
+        // here, the bot will take the assetids provided by the SteamBot, and compare them to the bot's inventory to find
+        // market_hash_names, then compare the market_hash_names to the BP.TF schema to find the price.
+
+        // BP.TF schema url for skin values (will later be on our file server, being updated every 5 minutes)
+        // http://backpack.tf/api/IGetMarketPrices/v1/?key=56cd0ca5b98d88be2ef9de16&appid=730
+    });
     socket.on('disconnect', function() {
         logger.info('Server > Disconnected socket ' + socket.id);
-		numConnected--;
-		console.log("Clients Connected: " + numConnected);
+        numConnected--;
+        console.log("Clients Connected: " + numConnected);
     })
 });
 server.listen(port);
