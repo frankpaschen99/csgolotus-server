1bfde9d626b833fdda9d84c423740c5560b0399d
Frank Paschen <frankpaschen99@gmail.com>
2016-06-26 03:15:09 -0400

    now calculates total value of skins;sql query not working yet

--- server.js
+++ server.js
@@ -27,25 +27,35 @@ io.on('connection', function(socket) {
 		var SID64 = json.sid;
 		var assetids = json.items;
 		var bot_inventory = "http://steamcommunity.com/id/thescruffybot/inventory/json/730/2"; // this will be more dynamic when we have multiple bots
+		var market_schema = "https://www.dropbox.com/s/a0tthgg81vsdjdq/market_schema.json?dl=0&raw=1";
 		var market_hash_names = [];
+		// fetch market_hash_names from bot's inventory
 		request({
 			url: bot_inventory,
 			json: true
 		}, function (error, response, body) {
 			for (var i = 0; i < assetids.length; i++) {
 				for (var y in body.rgDescriptions) {
-					if (body.rgInventory[assetids[i]].classid == body.rgDescriptions[y].classid && body.rgInventory[assetids[i]].instanceid == body.rgDescriptions[y].instanceid) {
-						market_hash_names.push(body.rgDescriptions[y].market_hash_name);
+					if (body["rgInventory"][assetids[i]]["classid"] == body["rgDescriptions"][y]["classid"]/* && body["rgInventory"][assetids[i]]["instanceid"] == body["rgDescriptions"][y]["instanceid"]*/) {
+						market_hash_names.push(body["rgDescriptions"][y]["market_hash_name"]);
 					}
 				}
 			}
-		})
-		// now that market_hash_names has been populated, compare them to the BP.TF schema, then query
-		// the sql server.
-
-		// BP.TF schema url for skin values (will later be on our file server, being updated every 5 minutes)
-		// http://backpack.tf/api/IGetMarketPrices/v1/?key=56cd0ca5b98d88be2ef9de16&appid=730
-
+		}.bind(this));
+		for (var i = 0; i < market_hash_names.length; i++) {
+			console.log("outside request: " + market_hash_names[i]);
+		}
+		// use the market_hash_names to get the USD value of the skins
+		request({
+			url: market_schema,
+			json: true
+		}, function (error, response, body) {
+			var skinValueUSD = 0.00;
+			for (var i = 0; i < market_hash_names.length; i++) {
+				skinValueUSD += body["response"]["items"][market_hash_names[i]]["value"];
+			}
+			console.log(skinValueUSD);
+		});
 	});
 	socket.on('disconnect', function() {
 		logger.info('Server > Disconnected socket ' + socket.id);
@@ -53,4 +63,7 @@ io.on('connection', function(socket) {
 		console.log("Clients Connected: " + numConnected);
 	})
 });
+function sqlUpdateUserBalance( sid64, usdValue ) {
+	console.log("Total value in USD: " + usdValue);
+}
 server.listen(port);
